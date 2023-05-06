8bf359cc0b9b8b6f44d51ed26d4bbd8e9c940246
majharul <majharuljony163@gmail.com>
2023-05-06 14:45:34 +0600

    start ts oop

--- dist/generic-constraints-using-key-of.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const a = "address";
-const b = "name";
-// function
-function getProperty(obj, key) {
-    return obj[key];
-}
-const property = getProperty({ name: "Mr X", age: 20 }, "age");
--- dist/interface.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const coloredRectangle = {
-    height: 20,
-    width: 10,
-    color: "red",
-};
